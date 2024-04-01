c814acca2bc0795d2416508df83f72320b197acb
SoufianeB01 <soufianeboufarache@gmail.com>
2024-04-01 03:37:23 +0200

    Loginscherm, Models, JSON service toegevoegd

---
+++ ADHD_App/Models/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json;
+
+namespace ADHD_App.Models
+{
+    public class Teacher : Person
+    {
+        public List<Child> Children { get; set; }
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Teacher>(this);
+        }
+    }
+}
