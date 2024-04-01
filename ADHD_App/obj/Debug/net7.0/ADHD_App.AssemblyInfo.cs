6ed082afdb2d5238287fb6649ebae5e43107cff3
DannyVroegop <danny436@hotmail.com>
2024-04-01 11:32:04 +0200

    testmerge

--- ADHD_App/obj/Debug/net7.0/ADHD_App.AssemblyInfo.cs
+++ ADHD_App/obj/Debug/net7.0/ADHD_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ac6dc6c5facf3e573df97cd66a5dfc06c86732")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
