c3875e9b480028b17b9a4c8613001e14ebbb426c
Timtimthethird <125371174+Timtimthethird@users.noreply.github.com>
2024-04-16 08:04:35 +0200

    Squashed commit of the following:
    commit bfbd7861e76229443a934f3ef6453d360cbdd1b3
    Author: DannyVroegop <danny436@hotmail.com>
    Date:   Mon Apr 1 11:44:46 2024 +0200
    
        t
    
    commit d37b426a799c704e69257ef38986c79d4c1ba177
    Author: DannyVroegop <danny436@hotmail.com>
    Date:   Mon Apr 1 11:43:53 2024 +0200
    
        Revert "testmerg2"
    
        This reverts commit 03407f02611a78ef4c0b1de85a4e60def9fdd50d.
    
    commit 5c4c063458a94e775d72746551656528431fafe9
    Merge: 6275b15 03407f0
    Author: DannyVroegop <danny436@hotmail.com>
    Date:   Mon Apr 1 11:43:09 2024 +0200
    
        test soufiane
    
    commit 6275b15ae487099fcc7b0551180bbf608cdc2438
    Author: DannyVroegop <danny436@hotmail.com>
    Date:   Mon Apr 1 11:33:55 2024 +0200
    
        test
    
    commit 03407f02611a78ef4c0b1de85a4e60def9fdd50d
    Author: DannyVroegop <danny436@hotmail.com>
    Date:   Mon Apr 1 11:32:16 2024 +0200
    
        testmerg2
    
    commit 6ed082afdb2d5238287fb6649ebae5e43107cff3
    Author: DannyVroegop <danny436@hotmail.com>
    Date:   Mon Apr 1 11:32:04 2024 +0200
    
        testmerge
    
    commit 46ac6dc6c5facf3e573df97cd66a5dfc06c86732
    Author: SoufianeB01 <soufianeboufarache@gmail.com>
    Date:   Mon Apr 1 11:06:49 2024 +0200
    
        Inlogscherm
    
    commit c814acca2bc0795d2416508df83f72320b197acb
    Author: SoufianeB01 <soufianeboufarache@gmail.com>
    Date:   Mon Apr 1 03:37:23 2024 +0200
    
        Loginscherm, Models, JSON service toegevoegd

---
+++ ADHD_App/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace ADHD_App.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Path { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Item>(this);
+        }
+    }
+}
