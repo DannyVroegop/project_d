835ca419425c8ba426f43936236ea8d73405657e
DannyVroegop <danny436@hotmail.com>
2024-04-24 12:12:14 +0200

    userinfo

--- ADHD_App/Models/Extra_userinfo.cs
+++ ADHD_App/Models/Extra_userinfo.cs
@@ -1,32 +1,36 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-
-namespace ADHD_App.Models
+﻿namespace ADHD_App.Models
 {
-    public class Extra_userinfo
+    public class Extra_userInfo
     {
-        public int Id { get; private set; }
-        public string[] unlockedImages { get; private set; }
-        public string ProfilePicture { get; private set; }
+        public int Id { get; set; } //Connect extra user info to the user id
+        public int Currency { get; private set; }
+        public List<string> profile_pictures { get; private set; }
+        public string C_Profile_Picture { get; private set; }
 
-        //public Extra_userinfo(int ID, string[] images, string profilepicture)
-        //{
-        //    id = ID;
-        //    unlockedImages = images;
-        //    ProfilePicture = profilepicture;
-        //}
 
-        public void changePicture(string picture)
-        {
-            if (unlockedImages.Contains(picture))
-            {
-                ProfilePicture = picture;
-            }
+        public Extra_userInfo(int id) {
+            Id = id;
+            //load the extra userinfo json and extra the info related to id
         }
 
-        public override string ToString()
+        public bool Purchase(int amount)
         {
-            return JsonSerializer.Serialize<Extra_userinfo>(this);
+            if (amount >= 0 && amount >= Currency ) 
+            {
+                Currency -= amount;
+                return true;
+            }
+            return false;
+        }
+
+        public bool changeProfilePicture(string new_profile) 
+        {
+            if (profile_pictures.Contains(new_profile)) 
+            {
+                C_Profile_Picture = new_profile;
+                return true;
+            }
+            return false;
         }
     }
 }
