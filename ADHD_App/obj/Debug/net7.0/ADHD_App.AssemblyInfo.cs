ff75fe98d9ed453787ab901862535a6fda78ff1f
DannyVroegop <danny436@hotmail.com>
2024-05-15 11:37:55 +0200

    Integrated extra_userinfo into Person

--- ADHD_App/obj/Debug/net7.0/ADHD_App.AssemblyInfo.cs
+++ ADHD_App/obj/Debug/net7.0/ADHD_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e617b64a637a1856fc6d39f56e6599a1ae16577a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22874723a2237c9527841be982a7b5e5f1efda99")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
