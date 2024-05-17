1bc4460e8589f3a7a740e8a78e5d84334fc07892
DannyVroegop <danny436@hotmail.com>
2024-05-17 10:41:23 +0200

    ???

--- ADHD_App/obj/Debug/net7.0/ADHD_App.AssemblyInfo.cs
+++ ADHD_App/obj/Debug/net7.0/ADHD_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfbd7861e76229443a934f3ef6453d360cbdd1b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
