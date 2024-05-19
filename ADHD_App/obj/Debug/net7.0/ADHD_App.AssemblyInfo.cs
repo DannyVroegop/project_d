173ddbfa1b53dd49cc884f2e19175541bf73d12d
DannyVroegop <danny436@hotmail.com>
2024-05-19 13:17:31 +0200

    Finished profile loading info

--- ADHD_App/obj/Debug/net7.0/ADHD_App.AssemblyInfo.cs
+++ ADHD_App/obj/Debug/net7.0/ADHD_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19bcb388ba8e148508f4dc34837b5d92f9867b43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce56c48f05cf6659315288b6839d0adef8e75b6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ADHD_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
